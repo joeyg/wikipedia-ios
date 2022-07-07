6b322109cafdc17820a9310f59e1e5ee06c999f6
Toni Sevener <tsevener@wikimedia.org>
2022-07-07 13:24:14 -0500

    Progress with buttons

--- Wikipedia/Code/ApplePayContentView.swift
+++ Wikipedia/Code/ApplePayContentView.swift
@@ -1,141 +1,73 @@
 import SwiftUI
 
-protocol ApplePayContentViewDelegate: AnyObject {
-    func didAppear()
+struct ApplePayPriceButton: View {
+    let text: String
+    let font = Font.body
+    var body: some View {
+        Button(text) {
+            print("Button tapped!")
+        }
+        .font(font)
+        .frame(width: 109, height: 46)
+        .foregroundColor(.black)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(.gray, lineWidth: 1)
+        )
+    }
 }
 
 struct ApplePayContentView: View {
     
     var body: some View {
-        VStack {
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
+        VStack(spacing: 16) {
+            HStack(spacing: 9) {
+                ApplePayPriceButton(text: "$10")
+                ApplePayPriceButton(text: "$20")
+                ApplePayPriceButton(text: "$30")
             }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
+            HStack {
+                ApplePayPriceButton(text: "$50")
+                ApplePayPriceButton(text: "$100")
+                ApplePayPriceButton(text: "$300")
             }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
-            
         }
+        
         .padding([.top, .bottom], 20)
     }
 }
 
 struct ApplePayContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ApplePayContentView()
+        Group {
+            ScrollView(.vertical, showsIndicators: true) {
+                ApplePayContentView()
+            }
+            .previewDevice(PreviewDevice(rawValue: "iPhone 13 mini"))
+            .previewDisplayName("iPhone 13 mini")
+            
+            ScrollView(.vertical, showsIndicators: true) {
+                ApplePayContentView()
+            }
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+            .previewDisplayName("iPhone SE")
+            
+            if #available(iOS 15.0, *) {
+                ScrollView(.vertical, showsIndicators: true) {
+                    ApplePayContentView()
+                }
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
+                .previewDisplayName("iPhone 13 Pro Max (landscape)")
+                .previewInterfaceOrientation(.landscapeLeft)
+            } else {
+                // Fallback on earlier versions
+            }
+                                 
+            ScrollView(.vertical, showsIndicators: true) {
+                                   ApplePayContentView()
+                               }
+                               .previewDevice(PreviewDevice(rawValue: "iPad Pro (12.9-inch) (4th generation)"))
+                               .previewDisplayName("iPad Pro (12.9-inch)")
+        }
     }
 }
